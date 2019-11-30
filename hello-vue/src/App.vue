74347f0d2dfcea7e819d84c315872b5eaf74447e
happylay <1803455421@qq.com>
2019-11-30 16:45:46 +0800

    vuex模块化

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script>
-  export default {
+ export default {
     name: 'App',
+    mounted() {
+      window.addEventListener('unload', this.saveState);
+    },
+    methods: {
+      saveState() {
+        // 模块化后，调用 state 的代码修改为 this.$store.state.user
+        sessionStorage.setItem('userState', JSON.stringify(this.$store.state.user));
+      }
+    }
   }
 </script>
